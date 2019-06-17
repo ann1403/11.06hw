913ff5845780b6250346bcf348b80f2534f9d2ea
ann1403 <32761721+ann1403@users.noreply.github.com>
2019-06-18 02:21:36 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+// function greeting(number) {
+//     alert('Your number: ' + number);
+// }
+
+// function processUserInput(callback) {
+//     let number = prompt('Please enter your number');
+//     callback(number);
+// }
+
+// processUserInput(greeting);
+
+
+
+
+
+
+// function get(callback) {
+//     let a = 5;
+
+//     callback(a);
+// }
+
+// function greeting(a) {
+//     alert('Your number: ' + a);
+// }
+// setTimeout(get, 1000);
+// greeting(get);
+// console.log(a)
+
+
+// function first() {
+//     setTimeout(function() { console.log("First"); }, 1000);
+// }
+
+// function second() {
+//     console.log("Second");
+// }
+
+// first();
+// second();
+
+
+
+let a;
+
+function plus() {
+    setTimeout("console.log(a)", 1000);
+}
+
+function getInput(callback) {
+    a = prompt('Please enter your number');
+    callback(a);
+}
+getInput(plus);
+
+
+
+// var allUserData = [];
+
+// // определение функции logStuff для вывода в консоль
+// function logStuff(userData) {
+//     if (typeof userData === 'string') {
+//         console.log(userData);
+//     } else if (typeof userData === 'object') {
+//         for (var item in userData) {
+//             console.log(item + ': ' + userData[item]);
+//         }
+//     }
+// }
+
+// // Функция, принимающая два параметра, одним из которых является коллбэк 
+// function getInput(options, callback) {
+//     allUserData.push(options);
+//     callback(options);
+// }
+
+// // Пример вызова функции getInput с коллбком
+// getInput({ name: 'Rich', speciality: 'JavaScript' }, logStuff);
